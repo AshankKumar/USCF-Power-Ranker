1b794bff35a62eebe33b1727bc11e610e54d17b5
LudwigVonPoopen <yohaw1@gmail.com>
2018-03-06 17:47:35 -0500

    Changed API and finished script for HSN Club
    Changed the API in use to gspread and completed a version of the progrma that at the moment only work for the WWP HSN Chess Club. A more interactive script for other clubs uses will be implemented soon, and a column that displays how people moved up and down every month will be added.

--- Player.py
+++ Player.py
@@ -1,12 +1,17 @@
 class Player:
 
     def __init__(self):
+        self.idNum = ""
         self.name = ""
         self.rating = 0
 
-    def __init__(self, n, r):
+    def __init__(self, idNum, n, r):
+        self.idNum = idNum
         self.name = n
         self.rating = r
+
+    def get_idNum(self):
+        return self.idNum
 
     def get_name(self):
         return self.name
@@ -15,9 +20,7 @@ class Player:
         return self.rating
 
     def __repr__(self):
-        return self.name + ", " + str (self.rating)
+        return self.idNum + ", " + self.name + ", " + str (self.rating)
 
     def set_rating(self, r):
         self.rating = r
-
-
--- PowerRanker.py
+++ PowerRanker.py
@@ -1,56 +1,15 @@
-from __future__ import print_function
-import httplib2
-import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
 
 from Player import Player
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
-
-
 def get_credentials():
-    """Gets valid user credentials from storage.
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
     return credentials
 
 def get_rating(idNum):
@@ -83,43 +42,54 @@ def sort_players(people):
 
     #change all ratings at zero back to unrated
     for i in people:
-        if(i.get_rating() == "0"):
+        if(i.get_rating() == 0):
             i.set_rating("Unrated")
 
     return sortedPlayers
 
 def main():
+    gc = gspread.authorize(get_credentials())
+
+    wks = gc.open("HSN Chess Club POWER RANKINGS 2017-2018").get_worksheet(1)
+
+    list_of_lists = wks.get_all_values()
+
+    list_of_lists.pop(0) #remove row with column names
 
     players = []
-    
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                    'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                              discoveryServiceUrl=discoveryUrl)
 
-    spreadsheetId = '1Dz1PjHX8mZjbX7DsePL54-BEWStnCJwn1iSrllmXIKs'
-    rangeName = 'Sheet1!A2:C'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        for row in values:
-            rate = get_rating(row[1])
-            players.append(Player(row[2], rate))
-            #print('%s, %s' % (row[2], rate)) 
+    for row in list_of_lists:
+        rate = get_rating(row[1])
+        players.append(Player(row[1], row[2], rate))
 
     sortedPlayers = sort_players(players)
 
-    print('Name, Rating:')
-    for i in sortedPlayers:
-        print(i)
+    print(sortedPlayers)
 
-    #write to google sheet
+    #current method is hard coded, need to change to any amount of players
+    #implement change from last week
+    for i in range(2, 32): #list_of_lists = wks.get_all_values(), then len(list_of_lists)
+        num = str (i)
+        cells = 'B'+num+":"+'D'+num
+        #print(cells)
+        cell_list = wks.range(cells)
+        j = 1 #player = people.pop()
+        player = sortedPlayers.pop(0) #change naming
+        print(player)
+        for cell in cell_list:
+            if(j == 1):
+                cell.value = player.get_idNum() #id people.get_ID()
+            if(j == 2):
+                cell.value = player.get_name() #name people.get_name()
+            if(j == 3):
+                cell.value = player.get_rating() #rating people.get_rating()
+            j += 1
+        wks.update_cells(cell_list)
+        
+    #print('ID, Name, Rating:')
+    #for i in sortedPlayers:
+        #print(i)
 
 if __name__ == '__main__':
     main()
+    
